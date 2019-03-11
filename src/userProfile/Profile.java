7ad626fe72a7a6fd65f50e038d11d602efff9d89
Mengxi Lei <leim5@mcmaster.ca>
2019-03-11 00:50:11 -0400

    Finishes profile, waiting on one function from Dan

--- src/userProfile/Profile.java
+++ src/userProfile/Profile.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 /**
  * Class representing the user's profile
  * @author Mengxi Lei
- * @version Created 2019/03/07, Last Modified 2019/03/08
+ * @version Created 2019/03/07, Last Modified 2019/03/11
  */
-class Profile {
+public class Profile {
 	
 	//Declare variables
 	private String profile;
@@ -21,48 +21,48 @@ class Profile {
 	 * @param name name of user
 	 * @param profile profile name
 	 */
-	protected Profile (String profile) {
+	public Profile (String profile) {
 		this.profile = profile;
 		taste = new ArrayList<String>();
 		wines = new ArrayList<Integer>();
 		priceRange = new int[2];
-		modified = true;
+		modified = false;
 	}
 
 	/**
 	 * Accessor method for profile name
 	 * @return profile name
 	 */
-	protected String getProfile() {
+	public String getProfile() {
 		return profile;
 	}
 	/**
 	 * Accessor method for user's favorite taste
 	 * @return taste as a array (not arraylist)
 	 */
-	protected String[] getTaste() {
+	public String[] getTaste() {
 		return (String[]) taste.toArray();
 	}
 	/**
 	 * Accessor method for user's favorite wines
 	 * @return favorite as a array (not arraylist)
 	 */
-	protected String[] getWines() {
-		return (String[]) wines.toArray();
+	public Integer[] getWines() {
+		return (Integer[]) wines.toArray();
 	}
 	/**
-	 * Accessor method for if the object is modified
-	 * @return true for modified, false otherwise
+	 * Accessor method for the user's price range
+	 * @return user's price range
 	 */
-	protected boolean getModified() {
-		return modified;
+	public int[] getPriceRange() {
+		return priceRange;
 	}
 	
 	/**
 	 * Add the given taste to profile
 	 * @param element String representing the taste being added
 	 */
-	protected void addTaste(String element) {
+	public void addTaste(String element) {
 		if (taste.indexOf(element) != -1)
 			return;
 		taste.add(element);
@@ -73,7 +73,7 @@ class Profile {
 	 * Delete the given taste from profile
 	 * @param element String representing the taste being deleted
 	 */
-	protected void deleteTaste(String element) {
+	public void deleteTaste(String element) {
 		int index = taste.indexOf(element);
 		if (index == -1)
 			return;
@@ -85,25 +85,49 @@ class Profile {
 	 * Add the given wine to profile
 	 * @param element String representing the wine being added
 	 */
-	protected void addWine(Integer element) {
+	public void addWine(Integer element) {
 		if (wines.indexOf(element) != -1)
 			return;
 		wines.add(element);
-		checkPriceChange();
 		modified = true;
+		checkPriceChange();
 	}
 	
 	/**
 	 * Delete the given wine from profile
 	 * @param element String representing the wine being deleted
 	 */
-	protected void deleteWine(String element) {
+	public void deleteWine(Integer element) {
 		int index = wines.indexOf(element);
 		if (index == -1)
 			return;
 		wines.remove(index);
-		checkPriceChange();
 		modified = true;
+		checkPriceChange();
+	}
+	
+	/**
+	 * Accessor method for if the profile have been modified since last save
+	 * @return True if yes, false otherwise
+	 */
+	protected boolean getModified() {
+		return modified;
+	}	
+	
+	/**
+	 * Manually set the modified variable of the profile
+	 * @param bool Boolean which the modified is being set to
+	 */
+	protected void setModified(boolean bool) {
+		modified = bool;
+	}
+	
+	/**
+	 * Manually set user's price range
+	 * @param array User's price range
+	 */
+	protected void setPriceRange(int[] array) {
+		priceRange = array;
 	}
 	
 	/**
--- src/userProfile/ProfileManager.java
+++ src/userProfile/ProfileManager.java
@@ -2,49 +2,105 @@ package userProfile;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Class for managing the profiles
  * @author Mengxi Lei
- * @version Created 2019/03/07, Last Modified 2019/03/08
+ * @version Created 2019/03/07, Last Modified 2019/03/11
  */
 public class ProfileManager {
 	
 	//Declare variables
 	private final static int MAX_SIZE = 10;
-	private final static String PROFILE_LIST = "profile.bin";
+	private final static String PROFILE_LIST = "profile/profile.bin";
 	private int size;
 	private String[] names;
 	private Profile profile;
-	private boolean modified;
 	
 	/**
 	 * Constructor method
 	 * Setup the profile by creating a new one or reading from an existing one
 	 */
 	public ProfileManager () {
-		modified = false;
+		//Declare variables
+		int userInput;
+		boolean doubleBreak;
+		Scanner input = new Scanner(System.in);
 		read();
 		while (true) {
+			//Create a new profile if no profile available
 			if (size == 0) {
 				System.out.println("There are no existing profile, please create a profile to preceed");
 				newProfile();
 				break;
 			}
+			//Choose between load a profile or delete a profile (if cannot create more profiles)
 			else if (size < MAX_SIZE) {
+				doubleBreak = false;
+				while(true) {
+					System.out.println("Enter the number corresponding to the option you want to choose:");
+					System.out.println("    0: Delete a profile");
+					for (int i = 0; i < size; i++)
+						System.out.println("    " + (i+1) + ": Load profile \"" + names[i] + "\"");
+					userInput = input.nextInt();
+					if (userInput == 0) {
+						deleteProfile();
+						break;
+					}
+					else if (userInput > 0 && userInput <= size) {
+						read(names[userInput-1]);
+						doubleBreak = true;
+						break;
+					}
+					else
+						System.out.println("Invalid input, please enter a valid choice.");
+				}
+				if (doubleBreak)
+					break;
 			}
+			//Choose between load a profile, create a new profile or delete a profile
 			else {
+				doubleBreak = false;
+				while(true) {
+					System.out.println("Enter the number corresponding to the option you want to choose:");
+					System.out.println("    0: Delete a profile");
+					for (int i = 0; i < size; i++)
+						System.out.println("    " + (i+1) + ": Load profile \"" + names[i] + "\"");
+					System.out.println("    " + (size+1) + ": Create a profile");
+					userInput = input.nextInt();
+					if (userInput == 0) {
+						deleteProfile();
+						break;
+					}
+					else if (userInput > 0 && userInput <= size) {
+						read(names[userInput-1]);
+						doubleBreak = true;
+						break;
+					}
+					else if (userInput == size+1) {
+						newProfile();
+						doubleBreak = true;
+						break;
+					}
+					else
+						System.out.println("Invalid input, please enter a valid choice.");
+				}
+				if (doubleBreak)
+					break;
 			}
 		}
-		
+		input.close();
 	}
 	
 	/**
 	 * Close the profile, make sure to output the profile is it have been modified
 	 */
 	public void close() {
-		if (modified)
+		if (profile.getModified())
 			writeProfile();
 	}
 	
@@ -72,13 +128,73 @@ public class ProfileManager {
 		input.close();
 	}
 	
+	/**
+	 * Load the profile of the given name
+	 * @param name name of profile being loaded
+	 */
 	private void read(String name) {
+		File file = new File("profile/" + name + ".bin");
+		Scanner input = null;
+		StringTokenizer[] inputArray = new StringTokenizer[3];
+		int length;
+		int[] price = new int[2];
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException exception) {
+			System.out.println("Cannot find " + name + ".bin");
+			System.exit(0);
+		}
+		profile = new Profile(name);
+		for (int i = 0; i < inputArray.length; i++)
+			inputArray[i] = new StringTokenizer(input.nextLine(), ",");
+		length = inputArray[0].countTokens();
+		for (int i = 0; i < length; i++)
+			profile.addTaste(inputArray[0].nextToken());
+		length = inputArray[1].countTokens();
+		for (int i = 0; i < length; i++)
+			profile.addWine(Integer.parseInt(inputArray[1].nextToken()));
+		price[0] = Integer.parseInt(inputArray[2].nextToken());
+		price[1] = Integer.parseInt(inputArray[2].nextToken());
+		profile.setPriceRange(price);
+		input.close();
 	}
 	
+	/**
+	 * Update the list of profiles to file
+	 */
 	private void write() {
+		File file = new File(PROFILE_LIST);
+		try {
+			FileWriter output = new FileWriter(file);
+			output.write(size + "\r\n");
+			for (int i = 0; i < size; i++)
+				output.write(names[i] + "\r\n");
+			output.close();
+		} catch (IOException exception) {}
 	}
 	
+	/**
+	 * Write the profile information to file
+	 */
 	private void writeProfile() {
+		String[] taste;
+		Integer[] wine;
+		int[] range;
+		File file = new File ("profile/" + profile.getProfile() + ".bin");
+		try {
+			FileWriter output = new FileWriter(file);
+			taste = profile.getTaste();
+			for (int i = 0; i < taste.length-1; i++)
+				output.write(taste[i] + ",");
+			output.write(taste[taste.length-1] + "\r\n");
+			wine = profile.getWines();
+			for (int i = 0; i < wine.length-1; i++)
+				output.write(wine[i] + ",");
+			output.write(wine[wine.length-1] + "\r\n");
+			range = profile.getPriceRange();
+			output.write(range[0] + "," + range[1]);
+			output.close();
+		} catch (IOException exception) {}
 	}
 	
 	/**
@@ -103,10 +219,38 @@ public class ProfileManager {
 		names[size] = name;
 		profile = new Profile(name);
 		size++;
-		modified = true;
+		profile.setModified(true);
 		write();
 		System.out.println("New profile created, the profile name is " + name + ".");
 		input.close();
+	}
+	
+	/**
+	 * Delete a existing profile
+	 */
+	private void deleteProfile() {
+		int userInput;
+		File file;
+		Scanner input = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter the number corresponding to the profile you want to delete:");
+			for (int i = 0; i < size; i++)
+				System.out.println("    " + i + ": \"" + names[i] + "\"");
+			userInput = input.nextInt();
+			if (userInput >= 0 && userInput < size) {
+				file = new File("profile/" + names[userInput] + ".bin");
+				file.delete();
+				for (int i = userInput; i < size-1; i++)
+					names[i] = names[i+1];
+				names[size-1] = null;
+				size--;
+				write();
+				input.close();
+				return;
+			}
+			else
+				System.out.println("Invalid input, please enter a valid choice.");
+		}
 	}
 	
 }
